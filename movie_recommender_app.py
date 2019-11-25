85d0dbfd2a0ed2d59c12c050c9fcab78fc7f2fd0
unknown <prakashbist11@gmail.com>
2019-11-25 17:30:13 +0545

    first commit

---
+++ movie_recommender_app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+
+##Read CSV File
+
+
+movie_user_likes = "Batman"
+
